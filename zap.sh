e3696bc4b01ae733bd362371e55a4d4365266b52
abhay9175 <113837646+abhay9175@users.noreply.github.com>
2023-12-19 15:26:09 +0530

    Update zap.sh

--- zap.sh
+++ zap.sh
@@ -10,7 +10,7 @@ echo $(id -u):$(id -g)
 
 
 # comment above cmd and uncomment below lines to run with CUSTOM RULES
-docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -c zap_rules -r zap_report.html
+docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t http://ec2-43-205-13-155.ap-south-1.compute.amazonaws.com:$PORT/v3/api-docs -f openapi -c zap_rules -r zap_report.html
 
 exit_code=$?
 
